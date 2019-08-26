8733293fc5e6701e4fe65b4e0831eafc33278534
Ubuntu <ubuntu@ip-172-31-32-169.ec2.internal>
2019-08-26 16:44:04 +0000

    add comments to the files | move JS from index to main js with update api links

--- run.py
+++ run.py
@@ -7,34 +7,32 @@ from collections import OrderedDict
 from operator import itemgetter
 
 app = Flask(__name__)
+
+# initial index page
 @app.route('/', methods=["GET","POST"])
 def index():
-    # if request.method == "GET":
-        # file = open("user_info.txt", "a")
-        # file.write("name:{}\n".format(request.form['username']))
-        # file.close()
     return render_template("index.html")
 
+#to dispaly the user name and save it in a file 
 @app.route('/user', methods=["GET","POST"])
 def display_user():
     my_arg = request.args
     user = my_arg['username']
-    print(user, "test name")
     file = open("user_info.txt", "a")
     file.write("name:{}\n".format(user))
     file.close()
     return jsonify(user)
-    
+
+#to get the user level    
 @app.route('/userlevel', methods=["GET","POST"])
 def show_selected_word():
     selected_word_riddle = ""
     my_arg = request.args
     user_level = my_arg['userlevel']
-    print (user_level)
     selected_riddle_word = create_riddle(user_level)
-    print ("selected_riddle_word ",selected_riddle_word)
     return  jsonify(selected_riddle_word)
 
+#to get and check the user answer
 @app.route('/answer', methods=["GET","POST"])
 def check_user_answer():
     my_arg2 = request.args
@@ -49,6 +47,7 @@ def check_user_answer():
     answer_result = check_user_input(user_answer_num, user_answer, scramble_selected_word, chances_counter, user_answers_list, user_name)
     return  jsonify(answer_result)
 
+#to determine if the user loses the game and save the score
 @app.route('/game_over', methods=["GET","POST"])
 def print_score():
     my_arg3 = request.args
@@ -56,18 +55,18 @@ def print_score():
     user_name = my_arg3['user_name']
     score = my_arg3['user_answers_list']
     save_user_score(user_name, score)
-    print(my_arg3, "my_arg3")
     return  jsonify(game_is_over)
 
+#to determine if the user win the game and save the score
 @app.route('/game_winner', methods=["GET","POST"])
 def game_winner():
     my_arg4 = request.args
     user_name = my_arg4['user_name']
     score = my_arg4['user_answers_list']
     save_user_score(user_name, score)
-    print("game_winner")
     return  jsonify(score)
 
+# to print the top scores
 @app.route('/top_ten')
 def top_ten():
     count = 0
@@ -82,16 +81,10 @@ def top_ten():
                 scores.append(int(line))
                 count +=1
         scores, names = zip(*sorted(zip(scores, names)))
-        print(names, "names")  
-        print(scores, "scores")         
-        
         scores = list(scores)
         names = list(names)
-        print(names, scores)
-        
         dictionary = dict(zip(names, scores))
         dictionary = dict(OrderedDict(sorted(dictionary.items(), key = itemgetter(1), reverse = True)))
-        print(dictionary)
     
     return render_template("top_ten.html", content = dictionary)
 
--- words.py
+++ words.py
@@ -3,15 +3,13 @@ import random
 import string
 import json
 
+# read all words from a file and put it in a list 
 def list_dec():
-    # with open("words_dictionary.json", "r") as file_json:
-    #     json_data = json.load(file_json)
-    #     json_data_list = list(json_data)
     with open("data/new_words.txt", "r") as file_json:
-        # json_data = json.load(file_json)
         json_data_list = file_json.readlines()
     return json_data_list
 
+# sorting words in different file depending on letters number 
 def sort_words():
     # three_letters_words_list = []
     # four_letters_words_list = []
@@ -45,14 +43,10 @@ def sort_words():
         words = words.replace("\n", "")
         all_posipole_words = list(itertools.permutations(words))
         for each_words in all_posipole_words:
-            # print(each_words, "each word")
             word_list.append(''.join(each_words))
-        # print(word_list, "test word")
-        # meaningful_list = []
         if word_list:
             for every_word in word_list:
                 if every_word in eight_letters_words_list:
-                    # print("every_word", every_word)
                     meaningful_list.append(every_word)
                     meaningful_list = list(set(meaningful_list))
                     
@@ -62,21 +56,5 @@ def sort_words():
             with open("data/eight_letters_meaning_words.json", "a") as file_json:
                 file_json.write(words + "\n")
     return True
-                
-        # print("all_posipole_words", all_posipole_words)
-    #     for i in range(0,len(all_posipole_words)):
-    #         word_list.append(''.join(all_posipole_words[i]))
-    #         print(word_list)
-    #     for word in word_list:
-    #         if word in three_letters_words_list:
-    #             temp_list.append(word)
-    #             if len(temp_list)>3:
-    #                 three_letters_meaning_words_list= list(set(three_letters_meaning_words_list + temp_list))
-    # # print(three_letters_meaning_words_list)
-    # with open("three_letters_meaning_words.json", "a") as file_json:
-    #     json.dump(three_letters_meaning_words_list, file_json)
-    # return three_letters_meaning_words_list
-                
-        
-                
+  
 # sort_words()
