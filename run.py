5c533a66f0d65bbb92ebcc1a266f4d7ec43c2d0d
Ubuntu <ubuntu@ip-172-31-32-169.ec2.internal>
2019-08-26 02:51:15 +0000

    remove scores from user scores file

--- run.py
+++ run.py
@@ -94,7 +94,7 @@ def top_ten():
         print(dictionary)
     
     return render_template("top_ten.html", content = dictionary)
-# print(top_ten())
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
